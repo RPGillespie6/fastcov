52cb997e78bce51d843abb22a78c701de176fd5b
Bryan Gillespie <rpgillespie6@gmail.com>
2019-08-03 16:47:28 -0400

    Test non-utf8 file encodings

---
+++ test/functional/cmake_project/src/latin1_enc.cpp
@@ -0,0 +1,34 @@
+// NOTE: ררר This file has been saved with the latin1 (ISO 8859-11) encoding ררר
+// Do not change the encoding or delete the non-utf8 characters
+
+#include <foobar.h>
+
+int latinfoo(bool take_branch)
+{
+    int x = 0;
+
+    for (int i = 0; i < 1000; i++) {
+        x += 2*i; //LCOV_EXCL_LINE
+
+        if (take_branch) //LCOV_EXCL_BR_LINE
+            x += i;
+    }
+
+    //LCOV_EXCL_START
+    for (int i = 0; i < 1000; i++) {
+        x -= 2*i;
+
+        if (take_branch)
+            x--;
+    }
+    //LCOV_EXCL_STOP
+
+    for (int i = 0; i < 1000; i++) {
+        x -= (i % 2) ? 1 : 2;
+
+        if (take_branch)
+            x++;
+    }
+
+    return x;
+}
---
+++ test/functional/cmake_project/test/latin1_test.cpp
@@ -0,0 +1,10 @@
+
+#include <foobar.h>
+
+using namespace std;
+
+int main()
+{
+    latinfoo(true);
+    return 0;
+}
