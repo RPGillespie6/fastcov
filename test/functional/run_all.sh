c3a7fedad218b87662639e054c5bb4bc07b3c2fe
Bryan Gillespie <rpgillespie6@gmail.com>
2019-08-06 15:56:21 -0400

    Add functional test for multi-test source touching

--- test/functional/run_all.sh
+++ test/functional/run_all.sh
@@ -33,7 +33,7 @@ test `find . -name *.gcda | wc -l` -eq 0
 ${TEST_DIR}/fastcov.py --gcov gcov-9 --zerocounters  # Clear previous test coverage
 ctest -R ctest_1
 
-# Test 1 (basic report generation - no branches)
+# Test (basic report generation - no branches)
 coverage run --append ${TEST_DIR}/fastcov.py --gcov gcov-9 --exclude test/ --lcov -o test1.actual.info
 cmp test1.actual.info ${TEST_DIR}/expected_results/test1.expected.info
 
@@ -43,7 +43,7 @@ ${TEST_DIR}/json_cmp.py test1.actual.gcov.json test1.actual.gcov.json #Just chec
 coverage run --append ${TEST_DIR}/fastcov.py --gcov gcov-9 --exclude test/ -o test1.actual.fastcov.json
 ${TEST_DIR}/json_cmp.py test1.actual.fastcov.json ${TEST_DIR}/expected_results/test1.expected.fastcov.json
 
-# Test 2 (basic report info - with branches)
+# Test (basic report info - with branches)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude test/ --lcov -o test2.actual.info
 cmp test2.actual.info ${TEST_DIR}/expected_results/test2.expected.info
 
@@ -53,19 +53,19 @@ ${TEST_DIR}/json_cmp.py test2.actual.gcov.json test2.actual.gcov.json #Just chec
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude test/ -o test2.actual.fastcov.json
 ${TEST_DIR}/json_cmp.py test2.actual.fastcov.json ${TEST_DIR}/expected_results/test2.expected.fastcov.json
 
-# Test 3 (basic lcov info - with branches; equivalent --include)
+# Test (basic lcov info - with branches; equivalent --include)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --include src/ --lcov -o test3.actual.info
 cmp test3.actual.info ${TEST_DIR}/expected_results/test2.expected.info
 
-# Test 4 (basic lcov info - with branches; equivalent --exclude-gcda)
+# Test (basic lcov info - with branches; equivalent --exclude-gcda)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude-gcda test1.cpp.gcda --lcov -o test4.actual.info
 cmp test4.actual.info ${TEST_DIR}/expected_results/test2.expected.info
 
-# Test 5 (basic lcov info - with branches; equivalent --source-files)
+# Test (basic lcov info - with branches; equivalent --source-files)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --source-files ../src/source1.cpp ../src/source2.cpp --lcov -o test5.actual.info
 cmp test5.actual.info ${TEST_DIR}/expected_results/test2.expected.info
 
-# Test 7 (gcov version fail)
+# Test (gcov version fail)
 if coverage run --append ${TEST_DIR}/fastcov.py --gcov ${TEST_DIR}/fake-gcov.sh ; then
     echo "Expected gcov version check to fail"
     exit 1
@@ -82,30 +82,37 @@ if [ "${ENCODING}" != "../src/latin1_enc.cpp: iso-8859-1" ]; then
     exit 1
 fi
 
-# Test 8 (lcov info - with non-utf8 encoding and fallback)
+# Test (lcov info - with non-utf8 encoding and fallback)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude test/ --fallback-encodings latin1 --lcov -o test8.actual.info
 cmp test8.actual.info ${TEST_DIR}/expected_results/latin1_test.expected.info
 
-# Test 9 (lcov info - with non-utf8 encoding and no fallback)
+# Test (lcov info - with non-utf8 encoding and no fallback)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude test/ --lcov -o test9.actual.info
 cmp test9.actual.info ${TEST_DIR}/expected_results/latin1_test.expected.info
-
 
 # Run ctest_3
 ${TEST_DIR}/fastcov.py --gcov gcov-9 --zerocounters # Clear previous test coverage
 ctest -R ctest_3
 
-# Test 10 (lcov info - with inclusive branch filtering)
+# Test (lcov info - with inclusive branch filtering)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude /usr/include test/ --include-br-lines-starting-with if else --lcov -o include_branches_sw.actual.info
 cmp include_branches_sw.actual.info ${TEST_DIR}/expected_results/include_branches_sw.expected.info
 
-# Test 11 (lcov info - with exclusive branch filtering)
+# Test (lcov info - with exclusive branch filtering)
 coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --exclude /usr/include test/ --exclude-br-lines-starting-with for if else --lcov -o exclude_branches_sw.actual.info
 cmp exclude_branches_sw.actual.info ${TEST_DIR}/expected_results/exclude_branches_sw.expected.info
 
-# Test 12 (lcov info - with smart branch filtering)
+# Test (lcov info - with smart branch filtering)
 coverage run --append ${TEST_DIR}/fastcov.py --branch-coverage --gcov gcov-9 --exclude /usr/include test/ --lcov -o filter_branches.actual.info
 cmp filter_branches.actual.info ${TEST_DIR}/expected_results/filter_branches.expected.info
+
+# Run ctest_all
+${TEST_DIR}/fastcov.py --gcov gcov-9 --zerocounters # Clear previous test coverage
+ctest # Run all
+
+# Test (multiple tests touching same source)
+coverage run --append ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --source-files ../src/source1.cpp --lcov -o multitest.actual.info
+cmp multitest.actual.info ${TEST_DIR}/expected_results/multitest.expected.info
 
 # Write out coverage as xml
 coverage xml -o coverage.xml
