a21d1ebfac604a01ced129750cfc984768937675
RomanBurtnyk <romankolomia1993@gmail.com>
2020-06-15 17:15:02 +0300

    FIXED: test fail

--- test/functional/run_all.sh
+++ test/functional/run_all.sh
@@ -168,12 +168,12 @@ test $rc -eq 6
 # Test (running data from other than build directory)
 RUN_DIR=${PWD}
 pushd ${TEST_DIR}
-COVERAGE_FILE=${RUN_DIR}/.coverage.other_dir coverage run -a ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --source-files cmake_project/src/source1.cpp --lcov -o ${RUN_DIR}/multitest_nonbuild_dir.actual.info
+COVERAGE_FILE=${RUN_DIR}/.coverage.other_dir coverage run -a ${TEST_DIR}/fastcov.py --exceptional-branch-coverage --gcov gcov-9 --source-files cmake_project/src/source1.cpp --lcov -p -o ${RUN_DIR}/multitest_nonbuild_dir.actual.info
 cmp ${RUN_DIR}/multitest_nonbuild_dir.actual.info ${TEST_DIR}/expected_results/multitest.expected.info
 popd
 
 # Test (error messages for missing sources)
-coverage run -a ${TEST_DIR}/fastcov.py -C ${TEST_DIR}/expected_results/missing_files.json --validate-sources -o missing.json | grep 'Cannot found' | grep "error" | wc -l > missing_files_count.log
+coverage run -a ${TEST_DIR}/fastcov.py -C ${TEST_DIR}/expected_results/missing_files.json --validate-sources -o missing.json | grep 'Cannot found' | grep "error" | wc -l 2> missing_files_count.log
 mfc=$(cat missing_files_count.log)
 test "$mfc" -eq "2"
 
