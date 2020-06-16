5fbb34f27c82bd7e21381131fcbc5d8fd8457212
RomanBurtnyk <romankolomia1993@gmail.com>
2020-06-17 01:55:15 +0300

    ADDED: dump coverage test

--- test/functional/run_all.sh
+++ test/functional/run_all.sh
@@ -194,7 +194,7 @@ cmp exclude_glob_test.actual.info ${TEST_DIR}/expected_results/exclude_glob_test
 
 # Test (print coverage info)
 coverage run -a ${TEST_DIR}/fastcov.py -C ${TEST_DIR}/expected_results/dump_coverage_test.json --lcov -o print_coverage.json -p 2>print_coverage.info.log
-cat print_coverage.info.log | grep 'Files Coverage'
+cat print_coverage.info.log | grep 'Files Coverage a'
 cat print_coverage.info.log | grep 'Functions Coverage'
 cat print_coverage.info.log | grep 'Lines Coverage'
 
